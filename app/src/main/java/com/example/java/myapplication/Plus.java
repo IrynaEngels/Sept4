ea73902c414e81fc3fe0738cb4e1370c3215d1f0
1 <1>
2017-09-04 19:52:55 +0300

    first commit

---
+++ app/src/main/java/com/example/java/myapplication/Plus.java
@@ -0,0 +1,60 @@
+package com.example.java.myapplication;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+@Deprecated
+public class Plus extends AppCompatTextView {
+
+    private static final String TAG = "TextViewPlus";
+
+    public Plus(Context context) {super(context);}
+
+    public Plus(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFont(context, attrs);
+    }
+
+    public Plus(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setFont(context, attrs);
+    }
+
+    private void setFont(Context context, AttributeSet attrs) {
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TextViewPlus);
+
+        String customFont = a.getString(R.styleable.TextViewPlus_font);
+
+        setFont(context, customFont);
+
+        a.recycle();
+
+    }
+
+    public boolean setFont(Context context, String asset) {
+
+        Typeface typeface = null;
+
+        try {
+
+            typeface.createFromAsset(context.getAssets(), asset);
+
+        } catch (Exception e) {
+
+            Log.e(TAG, "Could not set TypeFace : " + e.getMessage());
+            return false;
+
+        }
+
+        setTypeface(typeface);
+        return true;
+
+    }
+
+}
